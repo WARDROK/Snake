bcaf97330fa87a0804ee1f06fbfee79e83834352
Wardrok <xxstszydlik@gmail.com>
2024-08-01 03:01:32 +0200

    Add state manager

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+    sf::CircleShape shape(400.f);
     shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
